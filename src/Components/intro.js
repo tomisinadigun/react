46716df1d25ced67a94de2ae39c23284d7805112
tomisinadigun <adigunoluwatomisin9@gmail.com>
2023-05-23 09:02:02 +0100

    initial commit

---
+++ src/Components/intro.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+// const Introduction = (students) => {
+//     return (
+//         // <div>
+//         //     {
+//         //         students.map((name) => {
+//         //             <h2>My name is {name}. i am a react developer</h2>
+//         //         })
+//         //     }
+//         // </div>
+//     )
+// }
+
+
+// export default Introduction;
---
+++ src/Components/welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Welcome = () => {
+    return ( 
+        <div>
+            <h1>Welcome To GoMyCode</h1>
+        </div>
+    )
+}
+
+
+
+
+
+export default Welcome;
